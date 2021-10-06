d098e1b0bcc6318ed3ccc9ddaf83e0dbe4956ae9
Ren <43288246+Sam839@users.noreply.github.com>
2021-10-07 06:47:32 +0700

    Fixes

--- ClassFromRTTI/ClassFromRTTI.cpp
+++ ClassFromRTTI/ClassFromRTTI.cpp
@@ -18,8 +18,10 @@ typedef bool(__fastcall* fnIsTrue)(void* ecx, void* edx);
 typedef const char*(__fastcall* fnHelloWorld)(void* ecx, void* edx);
 
 int main() {
-	RTTI cRTTI1(true, true, true, true); // With caching + min iterations
-	RTTI cRTTI2; // Without caching
+	RTTI cRTTI1; // Default
+	RTTI cRTTI2(false, true, true, true); // With caching
+	RTTI cRTTI3(true); // With min iterations
+	RTTI cRTTI4(true, true, true, true); // With cache and min iterations
 	
 	HMODULE hTestDLL = LoadLibrary(TEXT("TestDLL.dll"));
 	if (!hTestDLL) {
@@ -37,58 +39,70 @@ int main() {
 	high_resolution_clock::time_point t1;
 	high_resolution_clock::time_point t2;
 
-	for (unsigned char i = 0; i < 10; i++) {
+	// Stage 1
+	t1 = high_resolution_clock::now();
+	for (unsigned char i = 0; i < 100; ++i) {
 		if (unTestingRVA != cRTTI1.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
 			printf("Error: Data error!\n");
 		}
 	}
 	t2 = high_resolution_clock::now();
-	printf("Bench (10 calls)                  = %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+	printf("Bench (100 calls)...............................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
 
 	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 10'000; i++) {
+	for (unsigned short i = 0; i < 1'000; ++i) {
 		if (unTestingRVA != cRTTI1.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
 			printf("Error: Data error!\n");
 		}
 	}
 	t2 = high_resolution_clock::now();
-	printf("Bench (10 000 calls)              = %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+	printf("Bench (1 000 calls).............................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
 
 	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 500'000; i++) {
+	for (unsigned int i = 0; i < 10'000; ++i) {
 		if (unTestingRVA != cRTTI1.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
 			printf("Error: Data error!\n");
 		}
 	}
 	t2 = high_resolution_clock::now();
-	printf("Bench (500 000 calls)             = %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+	printf("Bench (10 000 calls)............................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
 
 	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 1'000'000; i++) {
+	for (unsigned int i = 0; i < 50'000; i++) {
 		if (unTestingRVA != cRTTI1.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
 			printf("Error: Data error!\n");
 		}
 	}
 	t2 = high_resolution_clock::now();
-	printf("Bench (1 000 000 calls)           = %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+	printf("Bench (50 000 calls)............................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
 
+	// Stage 2
 	t1 = high_resolution_clock::now();
-	for (unsigned char i = 0; i < 10; i++) {
+	for (unsigned char i = 0; i < 100; ++i) {
 		if (unTestingRVA != cRTTI2.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
 			printf("Error: Data error!\n");
 		}
 	}
 	t2 = high_resolution_clock::now();
-	printf("Bench (10 calls + NoCache + NoMinIterations)        = %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+	printf("Bench (100 calls + Cache).......................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
 
 	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 10'000; i++) {
+	for (unsigned short i = 0; i < 1'000; ++i) {
 		if (unTestingRVA != cRTTI2.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
 			printf("Error: Data error!\n");
 		}
 	}
 	t2 = high_resolution_clock::now();
-	printf("Bench (10 000 calls + NoCache + NoMinIterations)    = %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+	printf("Bench (1 000 calls + Cache).....................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+
+	t1 = high_resolution_clock::now();
+	for (unsigned int i = 0; i < 10'000; ++i) {
+		if (unTestingRVA != cRTTI2.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
+			printf("Error: Data error!\n");
+		}
+	}
+	t2 = high_resolution_clock::now();
+	printf("Bench (10 000 calls + Cache)....................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
 
 	t1 = high_resolution_clock::now();
 	for (unsigned int i = 0; i < 50'000; i++) {
@@ -97,17 +111,82 @@ int main() {
 		}
 	}
 	t2 = high_resolution_clock::now();
-	printf("Bench (50 000 calls + NoCache + NoMinIterations)    = %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+	printf("Bench (50 000 calls + Cache)....................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
 
+	// Stage 3
 	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 100'000; i++) {
-		if (unTestingRVA != cRTTI2.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
+	for (unsigned char i = 0; i < 100; ++i) {
+		if (unTestingRVA != cRTTI3.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
 			printf("Error: Data error!\n");
 		}
 	}
 	t2 = high_resolution_clock::now();
-	printf("Bench (100 000 calls + NoCache + NoMinIterations)   = %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+	printf("Bench (100 calls + MinIterations)...............= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
 
+	t1 = high_resolution_clock::now();
+	for (unsigned short i = 0; i < 1'000; ++i) {
+		if (unTestingRVA != cRTTI3.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
+			printf("Error: Data error!\n");
+		}
+	}
+	t2 = high_resolution_clock::now();
+	printf("Bench (1 000 calls + MinIterations).............= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+
+	t1 = high_resolution_clock::now();
+	for (unsigned int i = 0; i < 10'000; ++i) {
+		if (unTestingRVA != cRTTI3.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
+			printf("Error: Data error!\n");
+		}
+	}
+	t2 = high_resolution_clock::now();
+	printf("Bench (10 000 calls + MinIterations)............= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+
+	t1 = high_resolution_clock::now();
+	for (unsigned int i = 0; i < 50'000; i++) {
+		if (unTestingRVA != cRTTI3.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
+			printf("Error: Data error!\n");
+		}
+	}
+	t2 = high_resolution_clock::now();
+	printf("Bench (50 000 calls + MinIterations)............= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+	
+	// Stage 4
+	t1 = high_resolution_clock::now();
+	for (unsigned char i = 0; i < 100; ++i) {
+		if (unTestingRVA != cRTTI4.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
+			printf("Error: Data error!\n");
+		}
+	}
+	t2 = high_resolution_clock::now();
+	printf("Bench (100 calls + Cache + MinIterations).......= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+
+	t1 = high_resolution_clock::now();
+	for (unsigned short i = 0; i < 1'000; ++i) {
+		if (unTestingRVA != cRTTI4.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
+			printf("Error: Data error!\n");
+		}
+	}
+	t2 = high_resolution_clock::now();
+	printf("Bench (1 000 calls + Cache + MinIterations).....= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+
+	t1 = high_resolution_clock::now();
+	for (unsigned int i = 0; i < 10'000; ++i) {
+		if (unTestingRVA != cRTTI4.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
+			printf("Error: Data error!\n");
+		}
+	}
+	t2 = high_resolution_clock::now();
+	printf("Bench (10 000 calls + Cache + MinIterations)....= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+
+	t1 = high_resolution_clock::now();
+	for (unsigned int i = 0; i < 50'000; i++) {
+		if (unTestingRVA != cRTTI4.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
+			printf("Error: Data error!\n");
+		}
+	}
+	t2 = high_resolution_clock::now();
+	printf("Bench (50 000 calls + Cache + MinIterations)....= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
+	
 #endif // BENCHMARK_TEST
 
 	if (!unTestingRVA) {
