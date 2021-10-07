e03301538e71860995415dc8f2958cf919b1840a
Ren <43288246+Sam839@users.noreply.github.com>
2021-10-07 09:20:22 +0700

    Update RTTI.cpp

--- ClassFromRTTI/RTTI.cpp
+++ ClassFromRTTI/RTTI.cpp
@@ -36,52 +36,6 @@ typedef struct _TYPEDESCRIPTOR {
 	char Name;
 } TYPEDESCRIPTOR, *PTYPEDESCRIPTOR;
 
-int CompareByteArray(PBYTE ByteArray1, PCHAR ByteArray2, PCHAR Mask, DWORD Length)
-{
-	DWORD nextStart = 0;
-	char start = ByteArray2[0];
-	for (DWORD i = 0; i < Length; i++)
-	{
-		if (Mask[i] == '?')
-		{
-			continue;
-		}
-		if (ByteArray1[i] == start)
-		{
-			nextStart = i;
-		}
-		if (ByteArray1[i] != (BYTE)ByteArray2[i])
-		{
-			return nextStart;
-		}
-	}
-	return -1;
-}
-
-PBYTE FindSignature(LPVOID BaseAddress, DWORD ImageSize, PCHAR Signature, PCHAR Mask)
-{
-	PBYTE Address = NULL;
-	PBYTE Buffer = (PBYTE)BaseAddress;
-
-	DWORD Length = strlen(Mask);
-
-	for (DWORD i = 0; i < (ImageSize - Length); i++)
-	{
-		int result = CompareByteArray((Buffer + i), Signature, Mask, Length);
-		if (result < 0)
-		{
-			Address = (PBYTE)BaseAddress + i;
-			break;
-		}
-		else
-		{
-			i += result;
-		}
-	}
-
-	return Address;
-}
-
 // Helpful functions
 static void* FindSignatureNative(unsigned char* pBegin, const unsigned char* pEnd, const char* szSignature) {
 	const size_t unSignatureLength = strlen(reinterpret_cast<const char*>(szSignature));
