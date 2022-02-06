a0cfaa71152c1d4696b442180bbb81ffda1eccd0
Ren <zeze839@gmail.com>
2022-02-07 01:59:01 +0700

    Hard simplification.

--- ClassFromRTTI/ClassFromRTTI.cpp
+++ ClassFromRTTI/ClassFromRTTI.cpp
@@ -21,193 +21,27 @@ typedef bool(__fastcall* fnIsTrue)(void* ecx, void* edx);
 typedef const char*(__fastcall* fnHelloWorld)(void* ecx, void* edx);
 
 int main() {
-	RTTI cRTTI1; // Default
-	RTTI cRTTI2(true, true, true, true, false); // With caching
-	RTTI cRTTI3(false, false, false, true, true); // With min iterations
-	RTTI cRTTI4(true, true, true, true, true); // With cache and min iterations
-	
 	HMODULE hTestDLL = LoadLibrary(TEXT("TestDLL.dll"));
 	if (!hTestDLL) {
 		printf("Error: TestDLL.dll not found.\n");
 		return -1;
 	}
 
-	printf("For start press <Enter>\n");
-	_CRT_UNUSED(getchar());
-
-	uintptr_t unTestingRVA = cRTTI2.GetVTableOffsetFromModule(hTestDLL, "TestingDLL");
-	printf("TestingDLL (RVA) = 0x%IX\n", unTestingRVA);
-
-#ifdef BENCHMARK_TEST
-	high_resolution_clock::time_point t1;
-	high_resolution_clock::time_point t2;
-
-	// Stage 1
-	printf("# Stage 1\n");
-	t1 = high_resolution_clock::now();
-	for (unsigned char i = 0; i < 100; ++i) {
-		if (unTestingRVA != cRTTI1.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (100 calls)...............................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned short i = 0; i < 1'000; ++i) {
-		if (unTestingRVA != cRTTI1.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (1 000 calls).............................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 10'000; ++i) {
-		if (unTestingRVA != cRTTI1.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (10 000 calls)............................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 50'000; i++) {
-		if (unTestingRVA != cRTTI1.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (50 000 calls)............................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	// Stage 2
-	printf("# Stage 2\n");
-	t1 = high_resolution_clock::now();
-	for (unsigned char i = 0; i < 100; ++i) {
-		if (unTestingRVA != cRTTI2.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (100 calls + Cache).......................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned short i = 0; i < 1'000; ++i) {
-		if (unTestingRVA != cRTTI2.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (1 000 calls + Cache).....................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 10'000; ++i) {
-		if (unTestingRVA != cRTTI2.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (10 000 calls + Cache)....................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 50'000; i++) {
-		if (unTestingRVA != cRTTI2.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (50 000 calls + Cache)....................= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	// Stage 3
-	printf("# Stage 3\n");
-	t1 = high_resolution_clock::now();
-	for (unsigned char i = 0; i < 100; ++i) {
-		if (unTestingRVA != cRTTI3.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (100 calls + MinIterations)...............= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned short i = 0; i < 1'000; ++i) {
-		if (unTestingRVA != cRTTI3.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (1 000 calls + MinIterations).............= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 10'000; ++i) {
-		if (unTestingRVA != cRTTI3.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (10 000 calls + MinIterations)............= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 50'000; i++) {
-		if (unTestingRVA != cRTTI3.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (50 000 calls + MinIterations)............= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-	
-	// Stage 4
-	printf("# Stage 4\n");
-	t1 = high_resolution_clock::now();
-	for (unsigned char i = 0; i < 100; ++i) {
-		if (unTestingRVA != cRTTI4.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (100 calls + Cache + MinIterations).......= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned short i = 0; i < 1'000; ++i) {
-		if (unTestingRVA != cRTTI4.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (1 000 calls + Cache + MinIterations).....= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 10'000; ++i) {
-		if (unTestingRVA != cRTTI4.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (10 000 calls + Cache + MinIterations)....= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-
-	t1 = high_resolution_clock::now();
-	for (unsigned int i = 0; i < 50'000; i++) {
-		if (unTestingRVA != cRTTI4.GetVTableOffsetFromModule(hTestDLL, "TestingDLL")) {
-			printf("Error: Data error!\n");
-		}
-	}
-	t2 = high_resolution_clock::now();
-	printf("Bench (50 000 calls + Cache + MinIterations)....= %lld ms\n", duration_cast<std::chrono::milliseconds>(t2 - t1).count());
-#endif // BENCHMARK_TEST
-
-	if (!unTestingRVA) {
-		printf("Error: TestingDLL RVA VTable not found.\n");
+	void* pTesting = FindRTTI(hTestDLL, ".?AVTestingDLL@@"); // ".?AV<NAME>@@"
+	if (!pTesting) {
+		printf("Error: TestingDLL VTable not found.\n");
 		return -1;
 	}
 
-	void** pTestingDLL_VT = reinterpret_cast<void**>(reinterpret_cast<char*>(hTestDLL) + unTestingRVA);
-
-	fnIsTrue IsTrue = reinterpret_cast<fnIsTrue>(pTestingDLL_VT[0]);
-	fnHelloWorld HelloWorld = reinterpret_cast<fnHelloWorld>(pTestingDLL_VT[1]);
+	void** pTestingVTable = reinterpret_cast<void**>(pTesting);
+	fnIsTrue IsTrue = reinterpret_cast<fnIsTrue>(pTestingVTable[0]);
+	fnHelloWorld HelloWorld = reinterpret_cast<fnHelloWorld>(pTestingVTable[1]);
 	
-	printf("# Result:\n");
-	printf("IsTrue = %s\n", IsTrue(pTestingDLL_VT, nullptr) ? "true" : "false");
-	printf("HelloWorld = %s\n", HelloWorld(pTestingDLL_VT, nullptr));
+	printf("RESULT:\n");
+	printf(" > hTestDLL (0x%IX)\n", reinterpret_cast<uintptr_t>(hTestDLL));
+	printf("  > pTestingVTable (0x%IX)\n", reinterpret_cast<uintptr_t>(pTestingVTable));
+	printf("   > IsTrue (0x%IX) = %s\n", reinterpret_cast<uintptr_t>(&(pTestingVTable[0])), IsTrue(pTestingVTable, nullptr) ? "true" : "false");
+	printf("   > HelloWorld (0x%IX) = %s\n", reinterpret_cast<uintptr_t>(&(pTestingVTable[1])), HelloWorld(pTestingVTable, nullptr));
 
 	return 0;
 }
--- ClassFromRTTI/RTTI.h
+++ ClassFromRTTI/RTTI.h
@@ -3,104 +3,18 @@
 #ifndef _RTTI_H_
 #define _RTTI_H_
 
-#define RTTI_DEFAULT_MAX_SYMBOL_LENGTH 0x7FF // 0x7FF - Max for MSVC
-
 // Default
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#include <DbgHelp.h>
 #include <Psapi.h>
-#include <NTSecAPI.h>
 
-#ifndef NT_SUCCESS
-#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
-#endif // !NT_SUCCESS
+// C++
+#include <cstdint>
 
-// Advanced
-#include <xmmintrin.h> // SSE
-#include <emmintrin.h> // SSE2
-#include <immintrin.h> // AVX, AVX2, FMA, FXSR, POPCNT, RDRAND, XSAVE
-#include <intrin.h>    // Default
-
-// Default libraries
-#pragma comment(lib, "dbghelp.lib")
-
-// STL
-#include <tuple>
-#include <string>
-#include <memory>
-#include <vector>
-
-// General definitions
-
-//  <ranges/modules/files>
-//   <module>
-//    <symbols>
-//     <symbol>:<address/offset>
-
-// Range of data
-typedef std::tuple<void*, void*> RangeOfDataForRTII;
-
-// Address
-typedef std::tuple<std::string, void*> SymbolAddress; // <symbol>:<address/offset>
-typedef std::vector<SymbolAddress> vecSymbolsAddresses; // <symbols>
-typedef std::tuple<RangeOfDataForRTII, vecSymbolsAddresses> RangeSymbolsAddresses; // <range>
-typedef std::tuple<HMODULE, vecSymbolsAddresses> ModuleSymbolsAddresses; // <module>
-typedef std::vector<RangeSymbolsAddresses> vecRangesSymbolsAddresses, *pvecRangesSymbolsAddresses; // <ranges>
-typedef std::vector<ModuleSymbolsAddresses> vecModulesSymbolsAddresses, *pvecModulesSymbolsAddresses; // <modules>
-
-// Offset
-typedef std::tuple<std::string, uintptr_t> SymbolOffset; // <symbol>:<address/offset>
-typedef std::vector<SymbolOffset> vecSymbolsOffsets; // <symbols>
-typedef std::tuple<RangeOfDataForRTII, vecSymbolsOffsets> RangeSymbolsOffsets; // <range>
-typedef std::tuple<HMODULE, vecSymbolsOffsets> ModuleSymbolsOffsets; // <module>
-typedef std::vector<RangeSymbolsOffsets> vecRangesSymbolsOffsets, *pvecRangesSymbolsOffsets; // <ranges>
-typedef std::vector<ModuleSymbolsOffsets> vecModulesSymbolsOffsets, *pvecModulesSymbolsOffsets; // <modules>
-
-//---------------------------------------------------------------------------------
-// RTTI interface
-//---------------------------------------------------------------------------------
-class RTTI {
-public:
-	RTTI(bool bCaching = false, bool bRangeCaching = false, bool bModulesCaching = false, bool bForceFastMethod = false, bool bMinIters = false);
-private:
-	// Finding Pattern
-	void* FindSignature(unsigned char* pBegin, const unsigned char* pEnd, const char* szSignature);
-private:
-	// Finding TypeInfo
-	void* FindTypeInfoAddressFromRange(void* pBegin, void* pEnd);
-public:
-	// Finding VTables
-	//  One
-	void* GetFastVTableAddressFromRange(void* pBegin, void* pEnd, const char* szClassName);
-	void* GetVTableAddressFromRange(void* pBegin, void* pEnd, const char* szClassName);
-	uintptr_t GetFastVTableOffsetFromRange(void* pBegin, void* pEnd, const char* szClassName);
-	uintptr_t GetVTableOffsetFromRange(void* pBegin, void* pEnd, const char* szClassName);
-	void* GetVTableAddressFromModule(HMODULE hModule, const char* szClassName);
-	uintptr_t GetVTableOffsetFromModule(HMODULE hModule, const char* szClassName);
-	//  Multiple
-	vecSymbolsAddresses GetVTablesAddressesFromRange(void* pBegin, void* pEnd);
-	vecSymbolsOffsets GetVTablesOffsetsFromRange(void* pBegin, void* pEnd);
-	vecSymbolsAddresses GetVTablesAddressesFromModule(HMODULE hModule);
-	vecSymbolsOffsets GetVTablesOffsetsFromModule(HMODULE hModule);
-private:
-	// Finding in cache
-	void* GetVTableAddressFromRangeCache(void* pBegin, void* pEnd, const char* szClassName);
-	uintptr_t GetVTableOffsetFromRangeCache(void* pBegin, void* pEnd, const char* szClassName);
-	void* GetVTableAddressFromModuleCache(HMODULE hModule, const char* szClassName);
-	uintptr_t GetVTableOffsetFromModuleCache(HMODULE hModule, const char* szClassName);
-private:
-	bool m_bAvailableSSE2;
-	bool m_bAvailableAVX2;
-	bool m_bCaching;
-	bool m_bRangesCaching;
-	bool m_bModulesCaching;
-	bool m_bForceFastMethod;
-	bool m_bMinIters;
-	vecRangesSymbolsAddresses m_vecRangesSymbolsAddressesCache;
-	vecModulesSymbolsAddresses m_vecModulesSymbolsAddressesCache;
-	vecRangesSymbolsOffsets m_vecRangesSymbolsOffsetsCache;
-	vecModulesSymbolsOffsets m_vecModulesSymbolsOffsetsCache;
-};
+// API
+void* FindRTTI(void* pBegin, void* pEnd, const char* szName);
+void* FindRTTI(HMODULE hModule, const char* szName);
+void* FindRTTI(const char* szModule, const char* szName);
+void* FindRTTI(const wchar_t* szModule, const char* szName);
 
 #endif // !_RTTI_H_
